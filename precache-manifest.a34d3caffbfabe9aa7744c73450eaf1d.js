15fa79b908e4bf2eee29faea3af8e58f9ac1567a
Sheng Chuang Wong <shengchuang.wong@grabtaxi.com>
2021-12-10 02:12:26 +0800

    first commit

---
+++ precache-manifest.a34d3caffbfabe9aa7744c73450eaf1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63ef68e115bac31e410cb3dfbde7a21",
+    "url": "/rinkeby-btd-token/index.html"
+  },
+  {
+    "revision": "ee4da5fd12d9e3d44a6b",
+    "url": "/rinkeby-btd-token/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7751ac1bdefe9bbbd7a4",
+    "url": "/rinkeby-btd-token/static/js/2.738dd687.chunk.js"
+  },
+  {
+    "revision": "ee4da5fd12d9e3d44a6b",
+    "url": "/rinkeby-btd-token/static/js/main.7ca30987.chunk.js"
+  },
+  {
+    "revision": "487b0ce2cc61450fc98b",
+    "url": "/rinkeby-btd-token/static/js/runtime-main.d0941165.js"
+  }
+]);
